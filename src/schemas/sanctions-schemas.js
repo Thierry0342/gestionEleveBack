89085520be0e3abcdbb00ad124aec347bcc0ea66
Thierry0342 <thrcasimir@gmail.com>
2025-09-25 08:27:15 +0300

    sanctions

--- src/schemas/sanctions-schemas.js
+++ src/schemas/sanctions-schemas.js
@@ -5,6 +5,9 @@ const SanctionSchema = sequelize.define("Sanction",
      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, },
       eleveId:{ type: DataTypes.INTEGER, allowNull: true, },
        sanction:{ type:DataTypes.BOOLEAN, allowNull:true, },
-        motif:{ type:DataTypes.STRING, allowNull:false, } });
+       motif: {
+          type: DataTypes.TEXT, 
+          allowNull: false,
+        }, });
 
         module.exports = SanctionSchema;
